ca0aa16cff7eaa3ec212199b3ce878966729198c
rameez <rameez012001@gmail.com>
2023-02-25 11:19:14 +0530

    corrections needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html','index-light.html','index-dark.html'],
   theme: {
     extend: {},
   },
